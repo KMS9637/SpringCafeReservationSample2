6db322cee38468b8680a337ae6bbbf6cfe5b5e47
이상용 <lsy3709@naver.com>
2024-08-30 12:23:36 +0900

    이미지 예측 기능 구현, 2) 사진첩에서 사진 가져와서 이미지뷰에 출력하기

--- app/src/main/java/com/sylovestp/firebasetest/testspringrestapp/AiPredictActivity.kt
+++ app/src/main/java/com/sylovestp/firebasetest/testspringrestapp/AiPredictActivity.kt
@@ -1,20 +1,69 @@
 package com.sylovestp.firebasetest.testspringrestapp
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
+import com.sylovestp.firebasetest.testspringrestapp.databinding.ActivityAiPredictBinding
+import com.sylovestp.firebasetest.testspringrestapp.databinding.ActivityJoinBinding
 
 class AiPredictActivity : AppCompatActivity() {
+    private lateinit var imageView: ImageView
+    private var imageUri: Uri? = null  // Nullable URI
+
+    private val selectImageLauncher: ActivityResultLauncher<Intent> = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == RESULT_OK) {
+            imageUri = result.data?.data!!
+//            imageView.setImageURI(imageUri)
+            // 코너 반경 (px 단위)
+            val cornerRadius = 500
+
+
+// 이미지 로드 및 코너 둥글게 적용
+            Glide.with(this)
+                .load(imageUri) // 이미지 URL 또는 로컬 리소스
+                .apply(RequestOptions().transform(RoundedCorners(cornerRadius)))
+                .into(imageView)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_ai_predict)
+
+        val binding = ActivityAiPredictBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        imageView = binding.userProfile2
+
+
+        binding.userProfile2.setOnClickListener {
+            openGallery()
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+    }// onCreate
+
+    private fun openGallery() {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        selectImageLauncher.launch(intent)
     }
+
 }
