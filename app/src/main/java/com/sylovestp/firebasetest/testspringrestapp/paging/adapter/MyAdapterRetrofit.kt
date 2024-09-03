676c210feb4e321c421e68a9c2f124722abc184e
이상용 <lsy3709@naver.com>
2024-09-03 16:07:28 +0900

    예측 뷰 원 모양 변경, 로그인 소프트 키보드 확인

--- app/src/main/java/com/sylovestp/firebasetest/testspringrestapp/paging/adapter/MyAdapterRetrofit.kt
+++ app/src/main/java/com/sylovestp/firebasetest/testspringrestapp/paging/adapter/MyAdapterRetrofit.kt
@@ -33,8 +33,8 @@ class MyAdapterRetrofit(val context: Context, val datas: List<UserItem>?)
         binding.itemPhone.text = item?.phone
         binding.itemAddress.text = item?.address
 
-//        val imageUrl = "http://10.100.201.87:8080/api/users/${item?.id}/profileImage"
-        val imageUrl = "http://192.168.219.200:8080/api/users/${item?.id}/profileImage"
+        val imageUrl = "http://10.100.201.87:8080/api/users/${item?.id}/profileImage"
+//        val imageUrl = "http://192.168.219.200:8080/api/users/${item?.id}/profileImage"
         Glide.with(context)
 
             .load(imageUrl)  // profileImageId는 URL 또는 리소스 ID일 수 있습니다
